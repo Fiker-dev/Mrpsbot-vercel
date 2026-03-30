bc7e6866ef219e7f9675f10d335310dc8d574433
Fiker-dev <fikerzabate16@gmail.com>
2026-03-30 10:28:55 +0200

    Change export syntax from ES6 to CommonJS

--- api/chat.js
+++ api/chat.js
@@ -8,7 +8,7 @@
 // - stop and submit immediately when Mr P says to pass it on
 // - respond naturally to greetings and small talk before moving into feedback mode
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
